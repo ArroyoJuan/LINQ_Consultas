96572bc6c8b7fd8b8492ae3e919265946315b435
Juank <Juank@DESKTOP-B4T6E7C>
2025-03-14 21:14:54 -0600

    Se desarrollo por consola

--- Program.cs
+++ Program.cs
@@ -12,20 +12,75 @@ namespace LINQ_Consultas
     {
         static void Main(string[] args)
         {
-            //Mapear la ruta del XML
-            string xmlRuta = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\LINQApp\Personas.xml");
+            string xmlRuta = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\LINQ_Consultas\Empleados.xml");
 
-            //Cargar el XML
             XDocument xmlDoc = XDocument.Load(xmlRuta);
 
-            //usar LINQ para filtrar personas del XML, que sean mayores de edad y que muestre el nombre.
-            var sennores = xmlDoc.Descendants("Persona").Where(x => (int?)x.Element("Edad") > 18);
-
-            foreach (var p in sennores)
+            var empleadosE30 = xmlDoc.Descendants("Empleado").Where(x => (int?)x.Element("Edad") > 30);
+            Console.WriteLine("Empleados mayores de 30 años: ");
+            foreach (var e in empleadosE30)
             {
-                Console.WriteLine("Datos: " + p.Value);
+                Console.WriteLine("Datos: " + e.Value);
             }
-
+            Console.WriteLine("");
+            Console.WriteLine("Empleados cuyo salario es mayor a 50000:");
+            var empleadosS50000 = xmlDoc.Descendants("Empleado").Where(x => (int?)x.Element("Salario") > 50000);
+            foreach (var e in empleadosS50000)
+            {
+                Console.WriteLine("Datos: " + e.Value);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Promedio de salario por departamento:");
+            var setDepartamento = Console.ReadLine();
+            var empleadosPS = xmlDoc.Descendants("Empleado").Where(x => x.Element("Departamento")?.Value == setDepartamento); //Va a filtrar el departamento
+            var suma = 0; //variable
+            var count = 0;
+            var promedioTotal=0;    
+            foreach (var e in empleadosPS) //Recorre los valores
+            {
+                suma = suma + (int)e.Element("Salario");
+                count++;
+            }
+            promedioTotal = suma / count;
+            Console.WriteLine("El salario promedio del departamento TI es: " + promedioTotal);
+            Console.WriteLine("");
+            Console.WriteLine("Empleado con el salario más alto:");
+            var TodosEmpleados = xmlDoc.Descendants("Empleado");
+            var salarioComparacion = 0;
+            foreach (var e in TodosEmpleados) //Recorre los valores
+            {
+                if ((int)e.Element("Salario") > salarioComparacion)
+                {
+                    salarioComparacion = (int)e.Element("Salario");
+                }
+            }
+            Console.WriteLine("El salario mayor es de: " + salarioComparacion);
+            Console.WriteLine("");
+            Console.WriteLine("Contar cuántos empleados hay por cada departamento:");
+            var empleadosTI = xmlDoc.Descendants("Empleado").Where(x => x.Element("Departamento")?.Value == "TI"); //Va a filtrar el departamento
+            var countTI = 0;
+            foreach (var e in empleadosPS) 
+            {
+                countTI++;
+            }
+            Console.WriteLine("En TI existen: " + countTI);
+            Console.WriteLine("");
+            var empleadosMar = xmlDoc.Descendants("Empleado").Where(x => x.Element("Departamento")?.Value == "Marketing"); //Va a filtrar el departamento
+            var countMar = 0;
+            foreach (var e in empleadosMar
+                )
+            {
+                countMar++;
+            }
+            Console.WriteLine("En Marketing existen: " + countMar);
+            Console.WriteLine("");
+            var empleadosVent = xmlDoc.Descendants("Empleado").Where(x => x.Element("Departamento")?.Value == "Ventas"); //Va a filtrar el departamento
+            var countVent = 0;
+            foreach (var e in empleadosVent)
+            {
+                countVent++;
+            }
+            Console.WriteLine("En Ventas existen: " + countVent);
             Console.ReadLine();
         }
     }
